cad2d8aef657aafcf352060675e11a77f2d8756d
Nagaraju Yasarapu <nagaraju.yasarapu@oracle.com>
2017-03-27 17:33:29 +0530

    fixed pagination of table in managing email section and added preloader..

--- HexiCloudAdmin/public_html/pages/manageemails/manageemails.js
+++ HexiCloudAdmin/public_html/pages/manageemails/manageemails.js
@@ -2,7 +2,12 @@
 define(['knockout',
     'config/serviceConfig',
     'ojs/ojcore',
-    'jquery',], function (ko, service, oj, $) {
+    'jquery',
+    'ojs/ojpagingcontrol',
+    'ojs/ojarraytabledatasource',
+    'ojs/ojpagingtabledatasource',
+    'ojs/ojcollectiontabledatasource'
+], function (ko, service, oj) {
     function manageEmailViewModel ()
     {
         var self = this;
@@ -18,22 +23,22 @@ define(['knockout',
         self.selectedRecordDescription = ko.observable('');
         self.isSelectedRecordResolved = ko.observable(false);
         self.selectedRecordSrId = ko.observable('');
-        self.recordDetailTableArray = ko.observableArray([]);
         self.allResolvedStatusList = ko.observableArray([
             {value: 'Y', label: 'Yes'},
             {value: 'N', label: 'No'}
         ]);
         self.selectedRecordResolvedStatus = ko.observable('');
         self.isSelectedRecordResolved = ko.observable(false);
-        self.selectedRecordSubject = ko.observable('');
+        self.selectedRecordMessage = ko.observable('');
         self.selectedRecordDescription = ko.observable('');
         
-         self.clearRecord = function() {
-                    self.selectedRecordSrId('');
-                    self.selectedRecordResolvedStatus([]);
-                    self.isSelectedRecordResolved(true);
-                    self.selectedRecordDescription('');
-                };
+        self.clearRecord = function() {
+            self.selectedRecordSrId('');
+            self.selectedRecordMessage('');
+            self.selectedRecordResolvedStatus([]);
+            self.isSelectedRecordResolved(true);
+            self.selectedRecordDescription('');
+        };
 
         self.initSearch = function (data, event) {
             self.clearRecord();
@@ -47,6 +52,7 @@ define(['knockout',
                     payload += '&requestId=' + self.requestId();
                 }
                 console.log(payload);
+                showPreloader();
                 service.findUserEmails(payload).then(searchSuccessFn, FailCallBackFn);
                 return;
             }
@@ -56,6 +62,7 @@ define(['knockout',
                     payload += '&requestId=' + self.requestId();
                 }
                 console.log(payload);
+                showPreloader();
                 service.findUserEmails(payload).then(searchSuccessFn, FailCallBackFn);
                 return;
             }
@@ -64,14 +71,19 @@ define(['knockout',
             }
             if (payload !== undefined) {
                 console.log(payload);
+                showPreloader();
                 service.findUserEmails(payload).then(searchSuccessFn, FailCallBackFn);
             } else {
+                showPreloader();
                 service.findUserEmails(null).then(searchSuccessFn, FailCallBackFn);
             }
         };
-         self.FailCallBackFn = function (xhr) {
-                    console.log(xhr);
-                };
+        
+        self.FailCallBackFn = function (xhr) {
+            console.log(xhr);
+            hidePreloader();
+        };
+        
         self.submitRecord = function (data, event) {
             console.log(self.selectedRecordResolvedStatus());
             if (self.selectedRecordResolvedStatus().length > 0) {
@@ -88,48 +100,53 @@ define(['knockout',
         };
         
         var submitRecordSuccessFn = function(data, status) {
-                    if (status === 'success') {
-                        console.log('Record updated');
-                        self.initSearch();
-                    }
-                };
+            if (status === 'success') {
+                console.log('Record updated');
+                self.initSearch();
+            }
+        };
         
-          var searchSuccessFn = function(data, status) {
-                    if (status !== 'nocontent') {
-                        console.log(data);
-                        self.recordDetailTableArray([]);
-                        var array = [];
-                        for (var idx = 0; idx < data.length; idx++) {
-                            array.push({
-                                srId: data[idx].srId,
-                                userId: data[idx].userId,
-                                subject: data[idx].subject,
-                                message: data[idx].message,
-                                sentTo: data[idx].sentTo,
-                                sentCC: data[idx].sentCC,
-                                sentBCC: data[idx].sentBCC,
-                                isResolved: data[idx].isResolved,
-                                csmEmailCount: data[idx].csmEmailCount,
-                                resolutionComments: data[idx].resolutionComments,
-                                createdDate: data[idx].createdDate});
-                        }
-                        self.recordDetailTableArray(array);
-                        self.recordsDatasource(new oj.ArrayTableDataSource(self.recordDetailTableArray));
-                    } else {
-                        console.log('Content not available for the selected step');
-                        self.recordDetailTableArray([]);
-                        self.recordsDatasource(new oj.ArrayTableDataSource(self.recordDetailTableArray));
-                    }
-                };
-                self.getRecord = function(data, event) {
-                    console.log(data);
-                    self.selectedRecordSrId(data.srId);
-                    var status = data.isResolved ? 'Y' : 'N';
-                    self.selectedRecordResolvedStatus(status);
-                    self.isSelectedRecordResolved(data.isResolved);
-                    self.selectedRecordSubject(data.subject);
-                    self.selectedRecordDescription(data.resolutionComments);
-                };
+        var searchSuccessFn = function(data, status) {
+            if (status !== 'nocontent') {
+                console.log(data);
+                var array = [];
+                for (var idx = 0; idx < data.length; idx++) {
+                    array.push({
+                        srId: data[idx].srId,
+                        userId: data[idx].userId,
+                        subject: data[idx].subject,
+                        message: data[idx].message,
+                        sentTo: data[idx].sentTo,
+                        sentCC: data[idx].sentCC,
+                        sentBCC: data[idx].sentBCC,
+                        isResolved: data[idx].isResolved,
+                        csmEmailCount: data[idx].csmEmailCount,
+                        resolutionComments: data[idx].resolutionComments,
+                        createdDate: data[idx].createdDate});
+                }
+//                self.recordsDatasource(new oj.ArrayTableDataSource(self.recordDetailTableArray));
+                self.recordsDatasource(new oj.PagingTableDataSource(new oj.ArrayTableDataSource(array)));
+                hidePreloader();
+            } else {
+                console.log('Content not available for the selected step');
+                self.recordsDatasource(new oj.PagingTableDataSource(new oj.ArrayTableDataSource([])));
+                hidePreloader();
+            }
+        };
+        
+        self.getRecord = function(data, event) {
+            console.log(data);
+            self.selectedRecordSrId(data.srId);
+            var status = data.isResolved ? 'Y' : 'N';
+            self.selectedRecordResolvedStatus(status);
+            self.isSelectedRecordResolved(data.isResolved);
+            self.selectedRecordMessage(data.message);
+            self.selectedRecordDescription(data.resolutionComments);
+        };
+        
+        self.handleBindingsApplied = function() {
+            self.initSearch();  
+        };
     };
     return manageEmailViewModel;
 
